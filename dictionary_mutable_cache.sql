315d0aa87d3dcb483cb614c8263a842fc9072c16
Ken Chen <zlchen.ken@gmail.com>
2025-01-10 23:36:41 -0800

    better

--- dictionary_mutable_cache.sql
+++ dictionary_mutable_cache.sql
@@ -22,7 +22,7 @@ LAYOUT(mutable_cache(database 'default' stream 'mysql_mutable_cache' update_from
 -- Test connectivity via dict_get for product id `pid_0001`
 SELECT dict_get('mysql_products_dict_mutable', 'name', 'pid_0001');
 
--- The join will first lookup keys in mutable stream, if the all keys are hit in mutable stream, 
+-- The join will first lookup keys in mutable stream, if the all keys are found in mutable stream, 
 -- the join can finish without falling back to do remote mysql lookup; otherwise it will send 
 -- remote fetch query to remote mysql for missing keys and stitch the two parts (keys from mutable 
 -- stream and keys from remtoe mysql) to finish the query. 
