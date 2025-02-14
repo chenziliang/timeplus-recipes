da94602e10052114721404a003a96f2506716750
Ken Chen <zlchen.ken@gmail.com>
2025-02-13 23:33:51 -0800

    mutable stream

--- mutable_stream.sql
+++ mutable_stream.sql
@@ -1,21 +1,52 @@
--- Separate key / value
+-- Mutable stream
 
-CREATE MUTABLE STREAM transactionS
+CREATE MUTABLE STREAM transactions
 (
   `id` string,
   `from_id` string,
   `to_id` string,
-  `value` uint256,
-  `meta` fixed_string(256),
+  `value` uint64,
   `status` string,
   `block_id` uint64
 )
 PRIMARY KEY id
-SETTINGS shards = 8, log_kvstore=true, logstore_codec='zstd', kvstore_options='max_background_jobs=6;max_write_buffer_number=4;enable_blob_files=true';
+SETTINGS 
+  shards = 8, 
+  logstore_codec='zstd', -- data compression in logstore 
+  log_kvstore=false, 
+  kvstore_options='max_background_jobs=6;max_write_buffer_number=4;enable_blob_files=false'; -- Tuning settings for underlying storage engine
+
+-- Data generation
+CREATE RANDOM STREAM r_transactions
+(
+  `id` string default 'id_' || to_string(rand64() % 100000000), 
+  `from_id` string default 'from_id_' || to_string(rand64() % 1000000), 
+  `to_id` string default 'to_id_' || to_string(rand64() % 1000000), 
+  `value` uint64, 
+  `status` string, 
+  `block_id` uint64
+)
+SETTINGS shards = 4;
 
 
--- Create secondary index
-ALTER STREAM transactions ADD INDEX sidx (from_id, to_id);
+-- Populate mutable stream with random data with approx 100 million unique keys 
+INSERT INTO transactions(* except(_tp_sn)) SELECT * FROM r_transactions LIMIT 100000000 SETTINGS eps=20e7;
 
--- Rebuild / refresh secondary index
-ALTER STREAM transactions MATERIALIZE INDEX sidx IN SHARD 0;
+-- Add secondary indexes for different columns for fast point / range query 
+ALTER STREAM transactions ADD INDEX from_idx (from_id); -- secondary index 
+ALTER STREAM transactions ADD INDEX to_idx (to_id); -- secondary index 
+ALTER STREAM transactions ADD INDEX ts_idx (_tp_time); -- secondary index 
+
+SELECT * FROM table(transactions) LIMIT 10;
+
+-- Point query against primary key `id` is fast
+SELECT * FROM table(transactions) WHERE id = 'id_10000006';
+
+-- Range query against primary key `id` is fast
+SELECT * FROM table(transactions) WHERE id >= 'id_10000006' AND id <= 'id_10000016';
+
+-- Point query against secondary key `from_id` is fast
+SELECT * FROM table(transactions) WHERE from_id = 'from_id_18';
+
+-- Range query against secondary key `from_id` is fast
+SELECT * FROM table(transactions) WHERE from_id >= 'from_id_18' AND from_id <= 'from_id_19' LIMIT 10 SETTINGS limit_hint=10;
--- troubleshooting.sql
+++ troubleshooting.sql
@@ -37,43 +37,42 @@ SETTINGS
 
 -- Replication status / lagging
 
-WITH sorted_recent_data_points AS
+CREATE OR REPLACE VIEW streams_replication_lagging
+AS
+WITH recent_replication_statuses AS
   (
     SELECT
-      node_id AS leader_node, name, state_string_value AS replication_statuses, _tp_time AS ts
+      node_id AS leader_node, database, name, dimension AS shard, json_extract_array_raw(state_string_value, 'shard_replication_statuses') AS replication_statuses, _tp_time AS ts
     FROM
-      table(system.stream_state_log)
+      system.stream_state_log
     WHERE
-      (state_name = 'quorum_replication_status') AND (_tp_time > (now() - 1m))
+      (_tp_time > (now() - 5m)) AND (state_name = 'quorum_replication_status')
     ORDER BY
       _tp_time DESC
-  ), latest_data_point AS
+    SETTINGS
+      query_mode = 'table'
+  ), 
+latest_replication_statuses AS 
+(
+  SELECT 
+    database, name, to_int(shard) AS shard, earliest(leader_node) AS leader_node, earliest(replication_statuses) AS replication_statuses, earliest(ts) AS ts 
+  FROM recent_replication_statuses
+  WHERE shard < 1000 
+  GROUP BY database, name, shard
+), 
+flatten AS
   (
     SELECT
-      leader_node, earliest(name) AS name, earliest(replication_statuses) AS replication_statuses, earliest(ts) AS ts
+      leader_node, database, name, shard, array_join(replication_statuses) AS status, ts
     FROM
-      sorted_recent_data_points
-    GROUP BY
-      leader_node
-  ), extracted AS
-  (
-    SELECT
-      leader_node, name, replication_statuses:shard AS shard, json_extract_array_raw(replication_statuses, 'shard_replication_statuses') AS statuses, ts
-    FROM
-      latest_data_point
-  ), flatten AS
-  (
-    SELECT
-      leader_node, name, shard, array_join(statuses) AS status, ts
-    FROM
-      extracted
+      latest_replication_statuses
   )
 SELECT
-  name, shard, leader_node, to_int(status:node) AS node, status:next_sn AS next_sn, status:replicated_sn AS replicated_sn, status:state AS state, status:append_message_flow_paused AS append_paused, status:inflight_messages AS inflight_messages, status:is_learner AS learner, status:recent_active AS recent_active
+  database, name, shard, leader_node, to_int(status:node) AS peer_node, status:next_sn AS next_sn, status:replicated_sn AS replicated_sn, status:state AS state, status:append_message_flow_paused AS append_paused, status:inflight_messages AS inflight_messages, status:is_learner AS learner, status:recent_active AS recent_active
 FROM
   flatten
 ORDER BY
-  node, shard ASC;
+  leader, shard ASC;
 
 
 
@@ -134,10 +133,9 @@ ORDER BY
   database, name, shard, node FORMAT CSV" > rep_lags.csv
 
 
-
 -- Failed materialized view 
 -- If a MV which is not running in last 5 minutes, report error
-CREATE VIEW v_failed_mvs 
+CREATE OR REPLACE  v_failed_mvs 
 AS
 WITH running_mvs_in_last_5m AS
 (
@@ -160,38 +158,50 @@ WHERE
 SETTINGS
   query_mode = 'table';
 
--- Large lagging >= 1000 MVs in last 5 minutes
-CREATE VIEW v_big_lag_mvs
+-- MVs with lagging >= 1000 for all sources in last 5 minutes
+CREATE OR REPLACE v_big_lag_mvs
 AS
 WITH last_5m_progressing_status AS
 (
   SELECT 
-    database, name, state_name, state_value, _tp_time AS ts
+    database, name, state_name, dimension, state_value, _tp_time AS ts
   FROM 
     system.stream_state_log
   WHERE 
     (_tp_time > (now() - 5m)) AND (state_name IN ('processed_sn', 'end_sn')) 
-  ORDER BY _tp_time DESC
+  ORDER BY _tp_time DESC -- order here to make sure we have latest state
   SETTINGS
     query_mode = 'table'
 ),
-latest_mv_lagging AS
+latest_mv_lagging_per_source AS
 (
   SELECT 
-    database, name, state_name, earliest(state_value) AS state_value, earliest(ts) AS ts
+    database, name, state_name, latest(state_value) AS state_value, earliest(ts) AS ts
+  FROM 
+    last_5m_progressing_status
+  GROUP BY database, name, state_name, dimension
+),
+mv_lagging_aggr_per_state AS
+( -- Aggregate all sources
+  SELECT 
+    database, name, state_name, sum(state_value) AS state_value, earliest(ts) AS ts
   FROM 
     last_5m_progressing_status
   GROUP BY database, name, state_name
-  ORDER BY database, name, state_name ASC 
 ),
-grouped_stats AS
+mv_lagging_aggr_per_mv AS
 (
   SELECT 
     database, name, group_array(state_name) AS state_names, group_array(state_value) AS state_values, earliest(ts) AS ts
   FROM
-    latest_mv_lagging 
+    mv_lagging_aggr_per_state 
   GROUP BY database, name
 )
-SELECT database, name, state_names[1] = 'end_sn' ?  state_values[1] : state_values[2] AS end_sn, state_names[2] = 'processed_sn' ?  state_values[2] : state_values[1] AS processed_sn, end_sn - processed_sn AS lag, ts 
-FROM grouped_stats
+SELECT 
+  database, name, 
+  state_names[1] = 'end_sn' ?  state_values[1] : state_values[2] AS end_sn, 
+  state_names[2] = 'processed_sn' ?  state_values[2] : state_values[1] AS processed_sn, 
+  end_sn - processed_sn AS lag, 
+  ts 
+FROM mv_lagging_aggr_per_mv
 WHERE lag >= 1000;
