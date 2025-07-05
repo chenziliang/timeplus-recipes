7a24de7f4ff2fdc084b7a298e4ec9751aa85bf9f
Ken Chen <zlchen.ken@gmail.com>
2025-07-05 10:43:36 -0700

    format insert

---
+++ format_insert.sql
@@ -0,0 +1,2 @@
+CREATE STREAM j(json json(max_dynamic_paths=1)) ENGINE = Memory;
+INSERT INTO j FORMAT JSONEachRow {"json" : {"a" : 42}}, {"json" : {"b" : "Hello"}}, {"json" : {"a" : [1, 2, 3], "c" : "2020-01-01"}}
