82ed86a4bede1218301420a9e040685b857b94b1
Ken Chen <zlchen.ken@gmail.com>
2025-08-28 10:13:47 -0700

    sql udf fixes

--- udf.sql
+++ udf.sql
@@ -14,14 +14,15 @@ CREATE FUNCTION null_least3 AS (p1, p2, p3) -> if (p1 is null and p2 is null and
 
 CREATE FUNCTION null_greatest3 AS (p1, p2, p3) -> if (p1 is null and p2 is null and p3 is null, null, greatest(coalesce(p1, 0), coalesce(p2, 0), coalesce(p2, 0)));
 
-CREATE FUNCTION file_path_history AS (n) -> if(empty(n),  [], array_concat([n], file_path_history_01((SELECT if(empty(old_path), Null, old_path) FROM git.file_changes WHERE path = n AND (change_type = 'Rename' OR change_type = 'Add') LIMIT 1))));
+-- file_path_history
+CREATE FUNCTION file_path_history AS (n) -> if(empty(n),  [], array_concat([n], file_path_history_01((SELECT if(empty(old_path), null, old_path) FROM git.file_changes WHERE path = n AND (change_type = 'Rename' OR change_type = 'Add') LIMIT 1))));
 
-CREATE FUNCTION file_path_history_01 AS (n) -> if(is_null(n), [], array_concat([n], file_path_history_02((SELECT if(empty(old_path), Null, old_path) FROM git.file_changes WHERE path = n AND (change_type = 'Rename' OR change_type = 'Add') LIMIT 1))));
+CREATE FUNCTION file_path_history_01 AS (n) -> if(is_null(n), [], array_concat([n], file_path_history_02((SELECT if(empty(old_path), null, old_path) FROM git.file_changes WHERE path = n AND (change_type = 'Rename' OR change_type = 'Add') LIMIT 1))));
 
-CREATE FUNCTION file_path_history_02 AS (n) -> if(is_null(n), [], array_concat([n], file_path_history_03((SELECT if(empty(old_path), Null, old_path) FROM git.file_changes WHERE path = n AND (change_type = 'Rename' OR change_type = 'Add') LIMIT 1))));
+CREATE FUNCTION file_path_history_02 AS (n) -> if(is_null(n), [], array_concat([n], file_path_history_03((SELECT if(empty(old_path), null, old_path) FROM git.file_changes WHERE path = n AND (change_type = 'Rename' OR change_type = 'Add') LIMIT 1))));
 
-CREATE FUNCTION file_path_history_03 AS (n) -> if(is_null(n), [], array_concat([n], file_path_history_04((SELECT if(empty(old_path), Null, old_path) FROM git.file_changes WHERE path = n AND (change_type = 'Rename' OR change_type = 'Add') LIMIT 1))));
+CREATE FUNCTION file_path_history_03 AS (n) -> if(is_null(n), [], array_concat([n], file_path_history_04((SELECT if(empty(old_path), null, old_path) FROM git.file_changes WHERE path = n AND (change_type = 'Rename' OR change_type = 'Add') LIMIT 1))));
 
-CREATE FUNCTION file_path_history_04 AS (n) -> if(is_null(n), [], array_concat([n], file_path_history_05((SELECT if(empty(old_path), Null, old_path) FROM git.file_changes WHERE path = n AND (change_type = 'Rename' OR change_type = 'Add') LIMIT 1))));
+CREATE FUNCTION file_path_history_04 AS (n) -> if(is_null(n), [], array_concat([n], file_path_history_05((SELECT if(empty(old_path), null, old_path) FROM git.file_changes WHERE path = n AND (change_type = 'Rename' OR change_type = 'Add') LIMIT 1))));
 
 CREATE FUNCTION file_path_history_05 AS (n) -> if(is_null(n), [], [n]);
