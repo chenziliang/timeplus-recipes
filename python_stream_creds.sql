3a76d9ed945a4901d1ccc229b9b321f4a54da7a3
Ken Chen <zlchen.ken@gmail.com>
2026-04-04 21:43:25 -0700

    python stream creds

--- python_stream_creds.sql
+++ python_stream_creds.sql
@@ -1,4 +1,6 @@
 -- SYSTEM INSTALL PYTHON PACKAGE 'proton-driver';
+-- INSERT INTO sink_to_timeplus SELECT 'hello world ' || to_string(number) FROM system.numbers LIMIT 10;
+
 create external stream sink_to_timeplus 
 (
     raw string
@@ -15,8 +17,6 @@ def init(config_):
     """
     config_ is a JSON string with the following fields:
     {
-        "host": "127.0.0.1",
-        "port": 8463,
         "database": "default",
         "user": "default",
         "password": ""
@@ -25,19 +25,24 @@ def init(config_):
     global timeplus_client
 
     config = json.loads(config_)
-    timeplus_client = client.Client(host='127.0.0.1', port=8463, **config)
+    timeplus_client = client.Client(
+        host='127.0.0.1', 
+        port=8463, 
+        user=__timeplus_local_api_user, # timeplusd inject these ephemeral credentials 
+        password=__timeplus_local_api_password, 
+        **config)
 
 def deinit():
     if timeplus_client is not None:
         timeplus_client.disconnect()
 
 def sink_to_timeplus(raw_cols):
-    timeplus_client.execute("INSERT INTO target_stream(raw) VALUES", [[raw_col] for raw_col in raw_cols])
+    timeplus_client.execute(f"INSERT INTO target_stream(raw) VALUES", [[raw_col] for raw_col in raw_cols])
 
 $$
 SETTINGS
     type='python',
     write_function_name='sink_to_timeplus',
     init_function_name='init',
-    init_function_parameters='{"user": "default", "password": "", "database": "default"}',
+    init_function_parameters='{"database": "default"}',
     deinit_function_name='deinit';
